6e431e17000729e87b220f0f62a45dd8284841c8
Swapnil Akulwar <91781600+swapya97@users.noreply.github.com>
2022-11-19 19:29:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Information from './components/Information';
+function App() {
+  return (
+    <div>
+    <Information/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Information.js
@@ -0,0 +1,22 @@
+const Information = () => {
+  return (
+    <div>
+      <h1>About US</h1>
+      <p>At Zappos.com, our purpose is simple:to live and deliver WOW. </p>
+      <p>
+        Twenty year ago, we began as a small online retailer that only sold
+        shoes.Today we still sell shoes- as well as clothing handbags,accesories
+        and more. That "more" is providing the very best customer experience and
+        compony culture. Weaim to inspire the word by showing it's possible to
+        simultaneously deliver happiness to customers ,employess, vender,
+        shareholder and the cummunity in a long-term. sustainable way.
+      </p>
+      <p>
+        We hope that in the future people wont't even realize we started selling
+        shoes online, Instead they'll know Zoppos as a service company taht just
+        happens to sell_____.
+      </p>
+    </div>
+  );
+};
+export default Information;
